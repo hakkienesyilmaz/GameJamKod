af424264b3b61401c778034a9d43779f3f45d6c7
Hakki Enes Yilmaz <hakkienesyilmaz@gmail.com>
2023-04-10 01:08:51 +0300

    test dosyası

---
+++ LevelScript.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);  
+    }
+}
